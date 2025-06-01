e3f6cf899acbea78639f6a9cf2bb0ec76f4afaad
Edson <59818164+AnJayAx@users.noreply.github.com>
2025-06-01 15:42:57 +0800

    updated to use google cloud run to host ai

--- app.py
+++ app.py
@@ -1,20 +1,10 @@
 from flask import Flask, render_template, request, jsonify
 import requests
-import pandas as pd
-import numpy as np
-import os
-import joblib
-import base64
-import cv2
-import json
-from tensorflow.keras import models
-from werkzeug.utils import secure_filename
-import tempfile
 
 app = Flask(__name__)
 
 # Define the Docker service URL
-AI_SERVICE_URL = "http://127.0.0.1:8000"  # Use Docker service name if in same network
+AI_SERVICE_URL = "https://docker-ai-685669070628.asia-southeast1.run.app"  # Use Docker service name if in same network
 
 @app.route('/')
 def home():
@@ -49,7 +39,6 @@ def caduceus():
                 'message': 'Symptoms received',
                 'symptoms_received': symptoms
             })
-    
     return render_template('caduceus.html', page_title="CADUCEUS", active_page="caduceus")
 
 @app.route('/analyze_symptoms', methods=['POST'])
@@ -63,14 +52,6 @@ def analyze_symptoms():
             'status': 'error',
             'message': f'Error connecting to AI service: {str(e)}'
         }), 500
-
-@app.route('/autoassess')
-def autoassess():
-    return render_template('autoassess.html', page_title="AUTOASSESS", active_page="asutoassess")
-
-@app.route('/gradescopev2')
-def gradescopev2():
-    return render_template('gradescopev2.html', page_title="GRADESCOPEV2", active_page="gradescopev2")
 
 @app.route('/analyze_image', methods=['POST'])
 def analyze_image():
@@ -89,6 +70,14 @@ def analyze_image():
             'status': 'error',
             'message': f'Error connecting to AI service: {str(e)}'
         }), 500
+
+@app.route('/autoassess')
+def autoassess():
+    return render_template('autoassess.html', page_title="AUTOASSESS", active_page="asutoassess")
+
+@app.route('/gradescopev2')
+def gradescopev2():
+    return render_template('gradescopev2.html', page_title="GRADESCOPEV2", active_page="gradescopev2")
 
 if __name__ == '__main__':
     app.run(debug=True)
